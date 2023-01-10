52baf21e4f39116da5a1c51af1065dbc8f65f15d
Mateusz Wygonny <mateuszwygonny@MacBook-Pro-Mateusz.local>
2023-01-10 17:59:21 +0000

    feat: deflip after picking two cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,21 +3,15 @@ import "./Card.css";
 
 type CardProps = Readonly<{
   card: string;
+  flipped: boolean;
+  onClick: () => void;
 }>;
-export const Card = ({ card }: CardProps) => {
-  const [turned, setTurned] = useState(false);
-  console.log(turned);
-  const contentClass = "content " + (turned ? "active" : "");
+export const Card = ({ card, flipped, onClick }: CardProps) => {
+  const contentClass = "content " + (flipped ? "active" : "");
   const frontClass = "cardFront";
   const backClass = "cardBack";
   return (
-    <div
-      className="card"
-      onClick={() => {
-        console.log("huh");
-        setTurned(!turned);
-      }}
-    >
+    <div className="card" onClick={onClick}>
       <div className={contentClass}>
         <div className={frontClass}>Front</div>
         <div className={backClass}>Back</div>
--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Card } from "./Card/Card";
 
 type GameboardProps = Readonly<{
@@ -5,15 +6,40 @@ type GameboardProps = Readonly<{
 }>;
 
 export const Gameboard = ({ size }: GameboardProps) => {
+  const correctGuess = () => {
+    setGuessedCards(guessedCards.concat(flippedCards));
+    incorrectGuess();
+  };
+  const incorrectGuess = () => {
+    setFlippedCards([]);
+  };
   const shuffle = (values: string[]) => {};
+  const [flippedCards, setFlippedCards] = useState<number[]>([]);
+  const [guessedCards, setGuessedCards] = useState<number[]>([]);
   const cards = ["a1", "a2", "b1", "b2", "c1", "c2", "d1", "d2"];
+  console.log(flippedCards.length);
   const shuffled = shuffle(cards);
+  if (flippedCards.length == 2) {
+    setTimeout(incorrectGuess, 1000);
+  }
   return (
     <ul>
-      {cards.map((card) => {
+      {cards.map((card, index) => {
+        const currFlipped = flippedCards.includes(index);
         return (
           <li key={card}>
-            <Card card={card} />
+            <Card
+              card={card}
+              flipped={currFlipped}
+              onClick={
+                !currFlipped && flippedCards.length < 2
+                  ? () => {
+                      console.log("huh");
+                      setFlippedCards(flippedCards.concat(index));
+                    }
+                  : () => {}
+              }
+            />
           </li>
         );
       })}
