586af21b4507309327ba4bde83aa147e6ed553ec
Mateusz Wygonny <onny.mat@gmail.com>
2023-02-22 11:28:36 +0000

    feat: added keyboard control

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,8 +11,25 @@ export const Card = ({ card, flipped, onClick, back, color }: CardProps) => {
   const contentClass = "content " + (flipped ? "active" : "");
   const frontClass = "cardFront";
   const backClass = "cardBack " + back;
+
+  const handleOnKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key == "Enter") {
+      onClick();
+    }
+    if (event.key == " ") {
+      event.preventDefault();
+      onClick();
+    }
+  };
   return (
-    <div className="card" onClick={onClick}>
+    <div
+      className="card"
+      tabIndex={0}
+      aria-pressed={flipped}
+      role="button"
+      onClick={onClick}
+      onKeyDown={(event) => handleOnKeyDown(event)}
+    >
       <div className={contentClass}>
         <div className={frontClass} style={{ backgroundColor: color }}>
           {color ? "" : card}
